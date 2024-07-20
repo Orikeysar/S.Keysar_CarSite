dec4a99a3418417de199b1928b421f51727d710d
Your Name <116570267+Orikeysar@users.noreply.github.com>
2024-07-20 20:02:12 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,25 +1,24 @@
+const cors = require('cors');
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
 const path = require('path');
-const fs = require('fs');
-require('dotenv').config(); // Add this line
-
 const carRoutes = require('./routes/carRoutes');
 const { initUploadDir } = require('./utils/multerConfig');
 
 const app = express();
-const port = process.env.PORT || 5000; // Use environment variable
+const port = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: 'https://s-keysar-car-site.vercel.app/' // Replace with your Vercel client URL
+  origin: 'https://s-keysar-car-site.vercel.app', // Update with your Vercel client URL
+  optionsSuccessStatus: 200
 }));
+
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 initUploadDir(); // Initialize uploads directory
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }); // Use environment variable
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use('/api/cars', carRoutes);
 
