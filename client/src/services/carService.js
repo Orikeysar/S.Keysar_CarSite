1f2b2cc59c65da6ccd30c220d196845bfdccbc09
Your Name <116570267+Orikeysar@users.noreply.github.com>
2024-07-21 20:17:35 +0300

    server fix and test images 3

--- client/src/services/carService.js
+++ client/src/services/carService.js
@@ -1,3 +1,4 @@
+// services/carService.js
 import axios from 'axios';
 
 const apiUrl = process.env.REACT_APP_API_URL;
@@ -8,15 +9,15 @@ export const getCars = async () => {
 };
 
 export const addCar = async (formData) => {
-  const response = await axios.post(`${apiUrl}/cars`, formData, {
+  const response = await axios.post(apiUrl, formData, {
     headers: {
-      'Content-Type': 'multipart/form-data',
-    },
+      'Content-Type': 'multipart/form-data'
+    }
   });
   return response.data;
 };
 
 export const deleteCar = async (id) => {
-  const response = await axios.delete(`${apiUrl}/cars/${id}`);
+  const response = await axios.delete(`${apiUrl}/${id}`);
   return response.data;
 };
